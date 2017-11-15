2a8cbfc22798773b16e0588ec9a39855673fcfce
Li Zhe <lizhe@lizhe.org>
2017-11-15 20:42:54 +0800

    Hacking Date

--- HackingDate.v
+++ HackingDate.v
@@ -1,10 +1,10 @@
 module hackingdate;
-  reg [23:0] today [0:1];
-  initial 
-    begin
-      $system("+%y-%m-%d  > date_file");
-      $readmemh("date_file", today[0]); 
-      $display(today); 
-      $finish ;
-    end
+    reg [23:0] today [0:1];
+    initial 
+        begin
+            $system("+%y-%m-%d  > date_file");
+            $readmemh("date_file", today[0]); 
+            $display(today); 
+            $finish;
+        end
 endmodule
