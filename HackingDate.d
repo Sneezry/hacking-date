b291cd314570cf7d19bd3a0e426800183a92bc49
Zhe Li <zlhe@microsoft.com>
2017-11-21 17:02:28 +0800

    d

--- HackingDate.d
+++ HackingDate.d
@@ -3,7 +3,7 @@ import std.datetime;
 
 void main(string[ ] args)
 {
-auto currentTime = Clock.currTime();
-auto timeString = currentTime.toISOExtString();
+    auto currentTime = Clock.currTime();
+    auto timeString = currentTime.toISOExtString();
     writeln(timeString[0 .. 10]);
 }
